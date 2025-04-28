fee99eacf63a3c6e4f8d84e9a3cead9f09744fda
yogeshpanta <yogeshpanta33@gmail.com>
2025-04-28 14:10:17 +0545

    updated text_generator file

--- RAG_Project/models/text_generator.py
+++ RAG_Project/models/text_generator.py
@@ -5,12 +5,10 @@ from transformers import pipeline
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 
-import logging
-import coloredlogs
-from RAG_Project.configs.logging_config import setup_logging
 
 from RAG_Project.prompts.prompt_templates import RagChatPrompt
 from RAG_Project.models.reranked import Reranker
+
 
 class TunedChatGeneration:
     def __init__(self, rag_system, model):
@@ -25,7 +23,7 @@ class TunedChatGeneration:
             model=self.model,
             tokenizer=self.tokenizer,
             max_new_tokens=100,
-            temperature=0.7
+            temperature=0.7,
         )
         return HuggingFacePipeline(pipeline=pipe)
 
@@ -42,14 +40,10 @@ class TunedChatGeneration:
 
         if use_ranked:
             docs = self.reranker.rerank(query=question, top_n=3)
-            return question_answering_chain.invoke({
-                "input": question,
-                "context": docs
-            })
+            return question_answering_chain.invoke({"input": question, "context": docs})
         else:
             rag_chain = create_retrieval_chain(retriever, question_answering_chain)
             return rag_chain.invoke({"input": question})
-
 
 
 # class TunedChatGeneration:
@@ -82,7 +76,6 @@ class TunedChatGeneration:
 #         question_answering_chain = create_stuff_documents_chain(llm, prompt)
 
 
-
 #         if use_ranked:
 #             reranked_docs = self.rag_system.reranked_docs(query=question, top_n=3)
 #             docs = [docs for docs in reranked_docs]
@@ -99,4 +92,3 @@ class TunedChatGeneration:
 #             return rag_chain.invoke({"input": question})["answer"]
 #         # response= rag_chain.invoke({"input":RagChatPrompt.question})
 #         # return response["context"][0].page_content
-
